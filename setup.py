21891840d97cc1518e5dcbec6b09b0d448cbb2a4
Anand-Applex <anandaluvala@gmail.com>
2023-05-05 13:57:12 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Mushroom",
+    version="0.0.0",
+    description="ML project",
+    author="Aluvala Anand", 
+    packages=find_packages(),
+    license="MIT"
+)
